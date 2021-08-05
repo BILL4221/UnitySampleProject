7994b17c558c961cad1c31a4544e1432742ceb81
Bill-Aniwat <aniwat@trueaxiongames.com>
2021-08-05 11:56:02 +0700

    - implement object which move circular

---
+++ Assets/Script/TestObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runnex.TestProject
+{
+    public class TestObject : MonoBehaviour
+    {
+        [SerializeField]
+        private float angularSpeed;
+        [SerializeField]
+        private float radius;
+        private float currentAngle;
+
+        private void Awake()
+        {
+            currentAngle = 0;
+        }
+        private void Update()
+        {
+            currentAngle += angularSpeed;
+            float x = Mathf.Cos(currentAngle) * radius;
+            float y = Mathf.Sin(currentAngle) * radius;
+            transform.position = new Vector3(x, y, 0);
+        }
+    }
+}
